b93e3b74913de30a0f2406f74447cd9811aafe6e
Shy Alon <shy.alon@gmail.com>
2024-06-21 17:25:07 +0300

    langchain

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from openai import OpenAI
+# from openai import OpenAI
+from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
-app = App(token=os.environ.get('SLACK_BOT_TOKEN'))
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+# llm = OpenAI(openai_api_key=openai_api_key, temperature=0.9)#, model="gpt-3.5-turbo"
+chat = ChatOpenAI(temperature=0)
+
+# The slack app
+slack_bot_key = os.getenv('SLACK_BOT_TOKEN')
+app = App(token=slack_bot_key)
 
 @app.message("")
 def message_hello(message, say):
-  response = process_message(message['text'])
-  say(f"Hi, <@{message['user']}>! {response}")
+  messages = [
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content=message['text'])
+  ]
+  response = chat(messages)
+  say(f"Hi, <@{message['user']}>! {response.content}")
 
-
-def process_message(txt):
-  client = OpenAI()
-# client.api_key = os.getenv('OPENAI_API_KEY')
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are an efficient assistant."},
-      {"role": "user", "content": txt}
-    ]
-  )
-  return (completion.choices[0].message)
 
 
 if __name__ == '__main__':
